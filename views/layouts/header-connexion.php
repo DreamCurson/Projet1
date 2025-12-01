b6a3c689e081c985debb336a2b842b824db629d8
dreamcurson <dreamcurson@outlook.com>
2025-12-01 12:34:24 -0500

    Modification navigation

--- views/layouts/header-connexion.php
+++ views/layouts/header-connexion.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Lord Stampee</title>
     <link rel="stylesheet" href="{{ asset }}css/main.css" />
-    <script type="module" src="{{ asset }}script/main.js"></script>
   </head>
 
     <header>
