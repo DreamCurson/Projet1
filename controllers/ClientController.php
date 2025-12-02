1f260eab6214c5a8f7b7366ba62717fe9876eb21
dreamcurson <dreamcurson@outlook.com>
2025-12-02 11:18:38 -0500

    Ajout de l'affichage de l'index backend pour avoir les informations de l'utilisateur connecter

--- controllers/ClientController.php
+++ controllers/ClientController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 use App\Providers\View;
+use App\Models\User;
 
 class ClientController{
     public function __construct() {
@@ -10,8 +11,21 @@ class ClientController{
         }
     }
 
-    public function index(){
-        return View::render("client/index");
+    public function index($id = null){
+        if ($id === null && isset($_SESSION['user_id'])) {
+            $id = $_SESSION['user_id'];
+        }else{
+            return View::render('login');
+        }
+
+        $utilisateur = new User;
+        $selectId = $utilisateur->selectId($id);
+
+        if ($selectId) {
+            return View::render("client/index", ['utilisateur' => $selectId, 'privilege_id' => $_SESSION['privilege_id']]);
+        }else{
+            return view::render('error');
+        }
     }
 
 }
