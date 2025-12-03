30ac08647cba050c9047bc320b1df372ffa993ec
dreamcurson <dreamcurson@outlook.com>
2025-12-03 08:22:48 -0500

    Ajout front end de la page profil avec les informations du compte (nom, email)

--- views/client/index.php
+++ views/client/index.php
@@ -1,1 +1,17 @@
 {{ include('layouts/header-principal.php') }}
+
+<div class="profile-card">
+    <h2>Votre profil</h2>
+
+    <div class="profile-field">
+        <label>Nom :</label>
+        <span>{{utilisateur.name}}</span>
+    </div>
+
+    <div class="profile-field">
+        <label>Email :</label>
+        <span>{{utilisateur.email}}</span>
+    </div>
+
+    <a href="editProfil"><button class="button--edit">Modifier vos informations</button></a>
+</div>
