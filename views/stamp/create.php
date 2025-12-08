97487e9a15818b964ea0d9bef01ed558addbaf05
dreamcurson <dreamcurson@outlook.com>
2025-12-08 10:39:27 -0500

    Création du formulaire en html avec classe BEM

--- views/stamp/create.php
+++ views/stamp/create.php
@@ -1,7 +1,53 @@
-<pre>{{ {
-    'user': user_idUser,
-    'colors': colors,
-    'conditions': conditions,
-    'contries': contries
+{{ include('layouts/header-principal.php') }}
+<h1>Ajouter un timbre</h1>
 
-}|json_encode(constant('JSON_PRETTY_PRINT')) }}</pre>
+<form class="formulaire-ajouter" method="POST">
+    <div class="formulaire-ajouter__field">
+        <label for="name" class="formulaire-ajouter__label">Nom du timbre:</label>
+        <input type="text" id="name" name="name" class="formulaire-ajouter__input" required>
+    </div>
+
+    <div class="formulaire-ajouter__field">
+        <label for="dateCreated" class="formulaire-ajouter__label">Date de création:</label>
+        <input type="date" id="dateCreated" name="dateCreated" class="formulaire-ajouter__input" required>
+    </div>
+
+    <div class="formulaire-ajouter__field">
+        <label for="dimension" class="formulaire-ajouter__label">Dimension:</label>
+        <input type="text" id="dimension" name="dimension" class="formulaire-ajouter__input" required>
+    </div>
+
+    <div class="formulaire-ajouter__field">
+        <label for="condition_idCondition" class="formulaire-ajouter__label">Condition du timbre:</label>
+        <select id="condition_idCondition" name="condition_idCondition" class="formulaire-ajouter__select" required>
+            {% for condition in conditions %}
+                <option value="{{ condition.idCondition }}" class="formulaire-ajouter__option">{{ condition.cond }}</option>
+            {% endfor %}
+        </select>
+    </div>
+
+    <div class="formulaire-ajouter__field">
+        <label for="contry_idContry" class="formulaire-ajouter__label">Pays du timbre:</label>
+        <select id="contry_idContry" name="contry_idContry" class="formulaire-ajouter__select" required>
+            {% for contry in contries %}
+                <option value="{{ contry.idContry }}" class="formulaire-ajouter__option">{{ contry.contry }}</option>
+            {% endfor %}
+        </select>
+    </div>
+
+    <div class="formulaire-ajouter__field">
+        <label for="color_idColor" class="formulaire-ajouter__label">Couleur du timbre:</label>
+        <select id="color_idColor" name="color_idColor" class="formulaire-ajouter__select" required>
+            {% for color in colors %}
+                <option value="{{ color.idColor }}" class="formulaire-ajouter__option">{{ color.color }}</option>
+            {% endfor %}
+        </select>
+    </div>
+
+    <div class="formulaire-ajouter__field">
+        <label for="draw" class="formulaire-ajouter__label">Dessin:</label>
+        <textarea id="draw" name="draw" class="formulaire-ajouter__textarea" rows="4" required></textarea>
+    </div>
+
+    <button type="submit" class="formulaire-ajouter__button">Ajouter le timbre</button>
+</form>
