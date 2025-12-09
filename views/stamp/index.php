f22b4168035e0fca933b93610f4526659a703bef
dreamcurson <dreamcurson@outlook.com>
2025-12-09 07:54:48 -0500

    Ajout du timbre dans l'index stamp de l'utilisateur

--- views/stamp/index.php
+++ views/stamp/index.php
@@ -1,4 +1,24 @@
 {{ include('layouts/header-principal.php') }}
+{% if stamps is not empty %}
+    <div class="stamp-list">
+        <h2>Vos timbres :</h2>
+
+        {% for stamp in stamps %}
+            <div class="stamp-card">
+
+                <h3>{{ stamp.name }}</h3>
+
+                <a class="btn" href="show?{{ stamp.idTimbre }}">
+                    Voir le timbre
+                </a>
+
+            </div>
+        {% endfor %}
+
+    </div>
+{% else %}
+    <p>Aucun timbre trouvé. Ajouté votre premier timbre</p>
+{% endif %}
 
 <div>
     <a href="stampCreate" class="button_basic">Ajouter un timbre</a>
